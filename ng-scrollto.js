8f6801155ef1a1b54f48fbd57a1767272401c3ad
winsik <winsik.cz@gmail.com>
2014-07-30 23:04:47 +0200

    service for scroll

--- ng-scrollto.js
+++ ng-scrollto.js
@@ -17,49 +17,56 @@
 //      <a scroll-to="section1" offset="60">Section 1 with 60px offset</a>
 //   </div>
 // </div>
-//
-//  angular.module('app', ['ngScrollTo']);
 
-angular.module("ngScrollTo",[])
-  .directive("scrollTo", ["$window", function($window){
+angular.module('ngScrollTo', []);
+
+angular.module("ngScrollTo")
+  .directive("scrollTo", ["ScrollTo", function(ScrollTo){
     return {
       restrict : "AC",
       compile : function(){
-
-        var document = $window.document;
         
-        function scrollInto(idOrName, offset) {//find element with the given id or name and scroll to the first element it finds
-          if(!idOrName) //move to top if idOrName is not provided
-            $window.scrollTo(0, 0);
-
-          //check if an element can be found with id attribute
-          var el = document.getElementById(idOrName);
-          if(!el) {//check if an element can be found with name attribute if there is no such id
-            el = document.getElementsByName(idOrName);
-
-            if(el && el.length)
-              el = el[0];
-            else
-              el = null;
-          }
-
-          if(el) { //if an element is found, scroll to the element
-            if (offset) {
-              var top = $(el).offset().top - offset;
-              window.scrollTo(0, top);
-            }
-            else {
-              el.scrollIntoView();
-            }
-          }
-          //otherwise, ignore
-        }
-
         return function(scope, element, attr) {
           element.bind("click", function(event){
-            scrollInto(attr.scrollTo, attr.offset);
+            scrollTo.idOrName(attr.scrollTo, attr.offset);
           });
         };
       }
     };
 }]);
+
+angular.module("ngScrollTo")
+  .service('ScrollTo', ['$window', function($window) {
+
+    this.idOrName = function (idOrName, offset) {//find element with the given id or name and scroll to the first element it finds
+        var document = $window.document;
+        
+        if(!idOrName) {//move to top if idOrName is not provided
+          $window.scrollTo(0, 0);
+        }
+
+        //check if an element can be found with id attribute
+        var el = document.getElementById(idOrName);
+        if(!el) {//check if an element can be found with name attribute if there is no such id
+          el = document.getElementsByName(idOrName);
+
+          if(el && el.length)
+            el = el[0];
+          else
+            el = null;
+        }
+
+        if(el) { //if an element is found, scroll to the element
+          if (offset) {
+            var top = $(el).offset().top - offset;
+            window.scrollTo(0, top);
+          }
+          else {
+            el.scrollIntoView();
+          }
+        }
+        
+        //otherwise, ignore
+      }
+
+  }]); 
